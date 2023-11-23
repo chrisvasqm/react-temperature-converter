9463f355215ca1dc8a0b4ee5f99bbbe2df21a3f1
chrisvasqm <christianvasquezmelo@gmail.com>
2023-11-23 11:34:55 -0500

    Make conversion logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,40 @@ import './App.css'
 function App() {
   const [temperature, setTemperature] = useState(0);
   const [convertion, setConversion] = useState('fahrenheit');
+  const [result, setResult] = useState(undefined);
 
   function handleSubmit(e: any) {
     e.preventDefault();
     console.log('Temperature', temperature);
+
+    if (convertion === 'fahrenheit')
+      setResult(celsiusToFahrenheit(temperature).toFixed(2));
+    else
+      setResult(fahrenheitToCelsius(temperature).toFixed(2));
   }
 
   function handleTemperatureChange(event: any) {
     const value = event.target.value;
 
-    if (!value)
+    if (!value || isNaN(parseInt(value, 10)))
       return;
+
 
     setTemperature(parseInt(event.target.value, 10));
   }
 
   function handleConversionChange(event: any) {
     setConversion(event.target.value);
+  }
+
+
+  function celsiusToFahrenheit(celsius: number) {
+    return (celsius * 9 / 5) + 32;
+  }
+
+
+  function fahrenheitToCelsius(fahrenheit: number) {
+    return (fahrenheit - 32) * 5 / 9;
   }
 
   return (
@@ -35,13 +52,19 @@ function App() {
           value={temperature}
         />
 
-        <select className='dropdown' defaultValue={convertion} onChange={handleConversionChange}>
+        <select
+          className='dropdown'
+          defaultValue={convertion}
+          onChange={handleConversionChange}
+        >
           <option value="fahrenheit">Celsius to Fahrenheit</option>
           <option value="celsius">Fahrenheit to Celsius</option>
         </select>
 
         <button className='button' type='submit'>Convert</button>
       </form>
+
+      {result && <h3>{result}</h3>}
     </>
   )
 }
