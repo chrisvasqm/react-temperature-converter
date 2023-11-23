cbf1e523051413941c1cd60fe2c4868d96fab1aa
chrisvasqm <christianvasquezmelo@gmail.com>
2023-11-23 09:49:51 -0500

    Store temperature

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [temperature, setTemperature] = useState(0);
 
   function handleSubmit(e: any) {
     e.preventDefault();
-    console.log('Submitted', e.target.value);
+    console.log('Temperature', temperature);
+  }
+
+  function handleTemperatureChange(event: any) {
+    setTemperature(parseInt(event.target.value));
   }
 
   return (
     <>
       <h1>Temperature Converter</h1>
       <form className='form' onSubmit={handleSubmit}>
-        <input className='temperature' type='number' placeholder="What's the temperature?"/>
+        <input
+          className='temperature'
+          type='number'
+          placeholder="What's the temperature?"
+          onChange={handleTemperatureChange}
+          value={temperature}
+        />
+
         <select className='dropdown' defaultValue="celsius">
           <option value="fahrenheit">Celsius to Fahrenheit</option>
           <option value="celsius">Fahrenheit to Celsius</option>
         </select>
-        <button className='button'>Convert</button>
+
+        <button className='button' type='submit'>Convert</button>
       </form>
     </>
   )
