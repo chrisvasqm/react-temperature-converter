5af45fccabaeee16db9fd4bde3eddcbea1e26f47
chrisvasqm <christianvasquezmelo@gmail.com>
2023-11-23 09:15:27 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    console.log('Submitted');
+  }
+
+  return (
+    <>
+      <h1>Temperature Converter</h1>
+      <form className='form' onSubmit={handleSubmit}>
+        <input className='temperature' type='number' />
+        <select className='dropdown' defaultValue="celsius">
+          <option value="celsius">Celsius</option>
+          <option value="fahrenheit">Fahrenheit</option>
+        </select>
+        <button className='button'>Convert</button>
+      </form>
+    </>
+  )
+}
+
+export default App
